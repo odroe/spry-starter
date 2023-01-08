a8f0196de93b11d83e6d59da1bae4e6b59a7035e
Seven Du <whine.whitish.0u@icloud.com>
2023-01-08 14:22:08 +0800

    chore: 🎉 Create project

---
+++ bin/spry_starter.dart
@@ -0,0 +1,5 @@
+import 'package:spry_starter/spry_starter.dart' as spry_starter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spry_starter.calculate()}!');
+}
---
+++ test/spry_starter_test.dart
@@ -0,0 +1,8 @@
+import 'package:spry_starter/spry_starter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
